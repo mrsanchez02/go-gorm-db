3584a8d3b02576e58a9ff13a4e617f7a25f141c8
Jose L. Sanchez <leandrosanchez.it@gmail.com>
2023-04-12 01:41:11 -0400

    Migrations

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/mrsanchez02/go-gorm-db/storage"
+import (
+	"github.com/mrsanchez02/go-gorm-db/model"
+	"github.com/mrsanchez02/go-gorm-db/storage"
+)
 
 func main() {
 	driver := storage.MySQL
 	storage.New(driver)
+
+	storage.DB().AutoMigrate(&model.Product{}, &model.InvoiceHeader{}, &model.InvoiceItem{})
 }
