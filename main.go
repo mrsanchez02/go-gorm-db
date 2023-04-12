712588b780ddf7e09523963b8c1c1765c7389b29
Jose L. Sanchez <leandrosanchez.it@gmail.com>
2023-04-12 01:49:46 -0400

    Create Gorm

--- main.go
+++ main.go
@@ -6,8 +6,15 @@ import (
 )
 
 func main() {
-	driver := storage.MySQL
+	driver := storage.Postgres
 	storage.New(driver)
 
-	storage.DB().AutoMigrate(&model.Product{}, &model.InvoiceHeader{}, &model.InvoiceItem{})
+	product1 := model.Product{Name: "Curso de Go 2023", Price: 120}
+	obs := "Testing with Go"
+	product2 := model.Product{Name: "Curso de Testing con Go", Price: 150, Observations: &obs}
+	product3 := model.Product{Name: "Curso de Python", Price: 200}
+
+	storage.DB().Create(&product1)
+	storage.DB().Create(&product2)
+	storage.DB().Create(&product3)
 }
